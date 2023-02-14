d662fb8fa9573c825b7c78a84612da140ba15a6b
http:jajek <keinsell@protonmail.com>
2023-02-14 20:25:49 +0100

    refactor: disable `administration.ts`

--- src/commands/administration.ts
+++ src/commands/administration.ts
@@ -9,28 +9,28 @@ if (!process.env.CHATGPT_AUTH_TOKEN || !process.env.CHATGPT_COOKIES) {
   throw Error("Could not find BOT_TOKEN in your environment");
 }
 
-@Discord()
-export class Hard {
-  @Slash({ description: "Ask something that you want to know...", name: "ask" })
-  async say(
-    @SlashOption({
-      description: "Ask something that you want to know...",
-      name: "ask",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    })
-    text: string,
-    interaction: CommandInteraction
-  ): Promise<void> {
-    console.log(`Catched message: ${text}`);
+// @Discord()
+// export class Hard {
+//   @Slash({ description: "Ask something that you want to know...", name: "hard" })
+//   async say(
+//     @SlashOption({
+//       description: "Ask something that you want to know...",
+//       name: "ask",
+//       required: true,
+//       type: ApplicationCommandOptionType.String,
+//     })
+//     text: string,
+//     interaction: CommandInteraction
+//   ): Promise<void> {
+//     console.log(`Catched message: ${text}`);
 
-    // interaction.deferReply();
+//     // interaction.deferReply();
 
-    await prisma.message.deleteMany();
-    await prisma.conversation.deleteMany();
+//     await prisma.message.deleteMany();
+//     await prisma.conversation.deleteMany();
 
-    // console.log(`Received response: ${response.message.content.parts[0]}`);
+//     // console.log(`Received response: ${response.message.content.parts[0]}`);
 
-    // interaction.editReply(response.message.content.parts[0]);
-  }
-}
+//     // interaction.editReply(response.message.content.parts[0]);
+//   }
+// }
