42ce8a920eee2d10a425f4d6c436869b621f4b95
http:jajek <keinsell@protonmail.com>
2023-02-14 21:59:18 +0100

    Revert "refactor: update `splitMessage`"
    This reverts commit b7e063a8f3a5985ae380b85c5628fc3756dd8655.

--- src/utils/splitter.ts
+++ src/utils/splitter.ts
@@ -1,33 +1,47 @@
-import MarkdownIt from "markdown-it";
+class StringBuilder {
+  private buffer: string[] = [];
+  private length = 0;
+
+  append(segment: string) {
+    this.buffer.push(segment);
+    this.length += segment.length;
+  }
+
+  toString() {
+    return this.buffer.join("");
+  }
+
+  getLength() {
+    return this.length;
+  }
+}
 
 const MAX_MESSAGE_LENGTH = 2000;
 
 export function splitMessage(message: string): string[] {
-  const md = new MarkdownIt();
-  const tokens = md.parse(message, {});
-
+  const lines = message.split("\n");
   const messages: string[] = [];
   let currentMessage = "";
   let currentCharCount = 0;
+  let inCodeBlock = false;
 
-  for (const token of tokens) {
-    const tokenString = md.renderer.render([token], md.options);
-    const tokenLength = tokenString.length;
-
-    if (currentCharCount + tokenLength > MAX_MESSAGE_LENGTH) {
+  for (const line of lines) {
+    const lineLength = line.length + 1; // add 1 for the newline character
+    if (inCodeBlock) {
+      currentMessage += line + "\n";
+      if (line.startsWith("```")) {
+        inCodeBlock = false;
+      }
+    } else if (line.startsWith("```")) {
+      inCodeBlock = true;
+      currentMessage += line + "\n";
+    } else if (currentCharCount + lineLength > MAX_MESSAGE_LENGTH) {
       messages.push(currentMessage);
-      currentMessage = tokenString;
-      currentCharCount = tokenLength;
+      currentMessage = line + "\n";
+      currentCharCount = lineLength;
     } else {
-      currentMessage += tokenString;
-      currentCharCount += tokenLength;
-    }
-
-    if (token.type === "fence") {
-      // If the token is a code block, start a new message after the code block ends
-      messages.push(currentMessage);
-      currentMessage = "";
-      currentCharCount = 0;
+      currentMessage += line + "\n";
+      currentCharCount += lineLength;
     }
   }
 
