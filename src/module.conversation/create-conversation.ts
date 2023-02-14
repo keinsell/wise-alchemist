07bed0c57aa10b05e604072b627e772064edc84d
http:jajek <keinsell@protonmail.com>
2023-02-14 14:42:08 +0100

    fix: find conversation and return it when exists

--- src/module.conversation/create-conversation.ts
+++ src/module.conversation/create-conversation.ts
@@ -4,6 +4,19 @@ export async function createConversation(properties: {
   id?: string;
   discordChannelId: string;
 }) {
+  if (properties.id) {
+    // Find one if not create and return
+    const converation = await prisma.conversation.findUnique({
+      where: {
+        id: properties.id,
+      },
+    });
+
+    if (converation != null) {
+      return converation;
+    }
+  }
+
   const converation = await prisma.conversation.create({
     data: {
       id: properties.id,
