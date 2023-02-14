32cb1e216441d3db4a5f715a697d46970a079e64
http:jajek <keinsell@protonmail.com>
2023-02-14 22:25:29 +0100

    Revert "use ai to resolve ai problems"
    This reverts commit 017dd498a4fa9acac49768e4f7276dcc403b7edd.

--- src/utils/splitter.ts
+++ src/utils/splitter.ts
@@ -19,52 +19,46 @@ class StringBuilder {
 const MAX_MESSAGE_LENGTH = 2000;
 
 export function splitMessage(message: string): string[] {
-  const chunkSize = MAX_MESSAGE_LENGTH;
-  const codeBlockRegex = /^```[\s\S]*?^```/gm;
-  const chunks: string[] = [];
+  const lines = message.split("\n");
+  const messages: string[] = [];
+  let currentMessage = "";
+  let inCodeBlock = false;
 
-  let match;
-  let lastIndex = 0;
-  while ((match = codeBlockRegex.exec(message))) {
-    const codeBlockStart = match.index;
-    const codeBlockEnd = codeBlockRegex.lastIndex;
-    if (codeBlockStart > lastIndex) {
-      // Add text before the code block
-      chunks.push(message.substring(lastIndex, codeBlockStart));
-    }
-    const codeBlock = message.substring(codeBlockStart, codeBlockEnd);
-    if (codeBlock.length <= chunkSize) {
-      // Add the code block as a single chunk
-      chunks.push(codeBlock);
-    } else {
-      // Split the code block into multiple chunks
-      const codeChunks = [];
-      let start = 0;
-      while (start < codeBlock.length) {
-        let end = Math.min(start + chunkSize, codeBlock.length);
-        const chunk = codeBlock.substring(start, end);
-        if (chunk.match(/^```/g)) {
-          // If the chunk starts with a new code block, remove the closing code block
-          const closingCodeBlockIndex = chunk.lastIndexOf("```");
-          codeChunks.push(chunk.substring(0, closingCodeBlockIndex));
-          end -= chunk.length - closingCodeBlockIndex;
-        } else if (chunk.match(/```$/g)) {
-          // If the chunk ends with a closing code block, remove the opening code block
-          const openingCodeBlockIndex = chunk.indexOf("```");
-          codeChunks.push(chunk.substring(openingCodeBlockIndex + 3));
-          end -= openingCodeBlockIndex + 3;
-        } else {
-          codeChunks.push(chunk);
-        }
-        start = end;
+  for (const line of lines) {
+    const lineLength = line.length + 1; // add 1 for the newline character
+    if (inCodeBlock) {
+      currentMessage += line + "\n";
+      if (line.startsWith("```")) {
+        inCodeBlock = false;
       }
-      chunks.push(codeChunks.join(""));
+    } else if (line.startsWith("```")) {
+      const remaining = MAX_MESSAGE_LENGTH - currentMessage.length;
+      if (remaining < 3) {
+        // need at least 3 characters for a closing code block delimiter
+        messages.push(currentMessage);
+        currentMessage = "";
+      }
+      inCodeBlock = true;
+      currentMessage += line + "\n";
+    } else if (currentMessage.length + lineLength > MAX_MESSAGE_LENGTH) {
+      // If the last line of the current message is part of a code block, remove it
+      if (inCodeBlock) {
+        const codeBlockRegex = /^```.*\n[\s\S]*```.*\n$/m;
+        const match = currentMessage.match(codeBlockRegex);
+        if (match) {
+          currentMessage = currentMessage.slice(0, match.index) + match[0];
+        }
+      }
+      messages.push(currentMessage);
+      currentMessage = line + "\n";
+    } else {
+      currentMessage += line + "\n";
     }
-    lastIndex = codeBlockEnd;
   }
-  if (lastIndex < message.length) {
-    // Add text after the last code block
-    chunks.push(message.substring(lastIndex));
+
+  if (currentMessage) {
+    messages.push(currentMessage);
   }
-  return chunks;
+
+  return messages;
 }
