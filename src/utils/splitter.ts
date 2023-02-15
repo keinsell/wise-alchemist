890c9448a5cbfab64096540b0e99d1b5931d843e
http:jajek <keinsell@protonmail.com>
2023-02-15 12:43:32 +0100

    refactor: update `splitMessage`

--- src/utils/splitter.ts
+++ src/utils/splitter.ts
@@ -1,53 +1,55 @@
-class StringBuilder {
-  private buffer: string[] = [];
-  private length = 0;
+function splitMarkdownString(input: string, limit: number): string[] {
+  const paragraphs = input.split("\n\n");
+  const output: string[] = [];
+  let currentString = "";
 
-  append(segment: string) {
-    this.buffer.push(segment);
-    this.length += segment.length;
+  for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i];
+
+    if (currentString.length + paragraph.length <= limit) {
+      currentString += paragraph + "\n\n";
+    } else if (isCodeBlock(paragraph)) {
+      currentString += paragraph + "\n\n";
+    } else {
+      let splitIndex = findLastNewlineIndex(
+        paragraph,
+        limit - currentString.length
+      );
+      if (splitIndex === -1) {
+        output.push(currentString);
+        currentString = paragraph + "\n\n";
+      } else {
+        currentString += paragraph.slice(0, splitIndex) + "\n\n";
+        output.push(currentString);
+        currentString = paragraph.slice(splitIndex) + "\n\n";
+      }
+    }
   }
 
-  toString() {
-    return this.buffer.join("");
+  if (currentString.length > 0) {
+    output.push(currentString);
   }
 
-  getLength() {
-    return this.length;
+  return output;
+}
+
+function isCodeBlock(paragraph: string): boolean {
+  const codeBlockRegex = /^`{3}.+[\r\n]+([\s\S]*?)\r?\n`{3}$/gm;
+  return codeBlockRegex.test(paragraph);
+}
+
+function findLastNewlineIndex(str: string, limit: number): number {
+  let splitIndex = -1;
+  for (let i = 0; i < Math.min(str.length, limit); i++) {
+    if (str[i] === "\n") {
+      splitIndex = i;
+    }
   }
+  return splitIndex;
 }
 
 const MAX_MESSAGE_LENGTH = 2000;
 
 export function splitMessage(message: string): string[] {
-  const lines = message.split("\n");
-  const messages: string[] = [];
-  let currentMessage = "";
-  let currentCharCount = 0;
-  let inCodeBlock = false;
-
-  for (const line of lines) {
-    const lineLength = line.length + 1; // add 1 for the newline character
-    if (inCodeBlock) {
-      currentMessage += line + "\n";
-      if (line.startsWith("```")) {
-        inCodeBlock = false;
-      }
-    } else if (line.startsWith("```")) {
-      inCodeBlock = true;
-      currentMessage += line + "\n";
-    } else if (currentCharCount + lineLength > MAX_MESSAGE_LENGTH) {
-      messages.push(currentMessage);
-      currentMessage = line + "\n";
-      currentCharCount = lineLength;
-    } else {
-      currentMessage += line + "\n";
-      currentCharCount += lineLength;
-    }
-  }
-
-  if (currentMessage) {
-    messages.push(currentMessage);
-  }
-
-  return messages;
+  return splitMarkdownString(message, MAX_MESSAGE_LENGTH);
 }
