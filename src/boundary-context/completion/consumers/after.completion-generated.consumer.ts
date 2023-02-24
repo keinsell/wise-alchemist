de1310cf5009755d283bfd239e54b3394a490fd3
http:jajek <keinsell@protonmail.com>
2023-02-25 00:08:27 +0100

    refactor: handle generated messages

---
+++ src/boundary-context/completion/consumers/after.completion-generated.consumer.ts
@@ -0,0 +1,31 @@
+import { InjectQueue } from '@nestjs/bull';
+import { Injectable } from '@nestjs/common';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { Queue } from 'bull';
+import { PromptCreatedEvent } from 'src/boundary-context/prompt/events/prompt-created/prompt-created.event';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.infra';
+import { CompletionTask } from '../processors/completion.processor';
+import { DiscordService } from 'src/infrastructure/discord/discord.service';
+import { CompletionGeneratedEvent } from '../events/completion-generated/completion-generated.event';
+import { TextChannel } from 'discord.js';
+
+@Injectable()
+export class AfterCompletionGeneratedConsumer {
+  constructor(private discord: DiscordService) {}
+
+  @OnEvent('prompt.created')
+  async afterPromptCreated(event: CompletionGeneratedEvent) {
+    // Add new task in queue for generation of content
+    // Produce this job as separate event called "GenerationQueuedEvent"
+    // ...
+
+    const deliveryTarget =
+      event.payload.messageFromConversation.discord_channel_id;
+
+    const channel = (await this.discord.channels.fetch(
+      deliveryTarget,
+    )) as unknown as TextChannel;
+
+    await channel.send(event.payload.generationMade.completion);
+  }
+}
