eb21c88a21723b15647856bc82668aaf339b1b8f
http:jajek <keinsell@protonmail.com>
2023-02-24 18:50:57 +0100

    fix: update broken `InfrastructureModule` import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './infrastructure/infrastructure.module.js';
+import { InfrastructureModule } from './infrastructure/infrastructure.module.js';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(InfrastructureModule);
   await app.listen(3000);
 }
 
