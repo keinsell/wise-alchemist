6f809c0483a76d924f3fff57b25fc31d60ae3627
http:jajek <keinsell@protonmail.com>
2023-03-07 07:50:33 +0100

    feat: add mezmo logger

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,12 +3,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { DiscordModule } from '../application/discord/infra/discord.application.module';
 import { BullModule } from '@nestjs/bull';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AfterMessageAuthorizedCustomer } from 'src/boundary-context/prompt/consumers/after.message-authorized.consumer';
-import { EventStorageConsumer } from './event-storage/event-storage.consumer';
 import { EventStorageModule } from './event-storage/event-storage.module';
 import { PromptModule } from 'src/boundary-context/prompt/infrastructure/prompt.module';
 import { CompletionModule } from 'src/boundary-context/completion/infrastructure/completion.module';
 import * as redisurl from 'redis-url';
+import { MezmoLoggerModule } from './logger/mezmo-logger-module';
 
 @Module({
   imports: [
@@ -21,6 +20,7 @@ import * as redisurl from 'redis-url';
     PromptModule,
     EventStorageModule,
     CompletionModule,
+    MezmoLoggerModule,
     BullModule.forRootAsync({
       useFactory: () => {
         const redisConfig = redisurl.parse(process.env.REDIS_URL);
