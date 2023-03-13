6bd0f6b6a7d8a18bab27ed8bd4b922071b631603
Jakub Olan <keinsell@protonmail.com>
2023-03-13 17:52:01 +0100

    feat: add sentry interceptor (ENG-24) (#33)

---
+++ src/infrastructure/sentry/sentry.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import * as Sentry from '@sentry/node';
+
+const enableSentry = (err) => {
+  Sentry.captureException(err);
+  return throwError(err);
+};
+
+@Injectable()
+export class SentryInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(catchError(enableSentry));
+  }
+}
